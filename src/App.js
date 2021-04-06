d456472dc28cb06ed1e9fdc1d988a6cdffffc98b
victoria ukaonu <73253494+Sozchi@users.noreply.github.com>
2021-04-07 00:45:26 +0100

    list of countries

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import React from "react";
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props)
+{
+  let data = props.data;
+  let countryList = data.map( ( list ) => 
+    <li key={list.numericCode} className="allCountries">{list.name}</li>
+  );
   return (
     <div className="App">
-      <header className="App-header">
+      <ul>{countryList}</ul>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +24,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import data from './data/CountriesAll.json'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App data={data} />
   </React.StrictMode>,
   document.getElementById('root')
 );
